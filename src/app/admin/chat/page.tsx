8f9fd8b4d8b60c05d9aae9f86c302f1cce26c8a8
yechayo <2628505364@qq.com>
2026-04-08 19:31:32 +0800

    feat: replace chat page with TaskPanel

--- src/app/admin/chat/page.tsx
+++ src/app/admin/chat/page.tsx
@@ -3,27 +3,17 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useAdmin } from "@/hooks/useAdmin";
-import { McpChatProvider, useMcpChat } from "@/contexts/McpChatContext";
-import ChatSidebar from "@/components/chat/ChatSidebar";
-import ChatWindow from "@/components/chat/ChatWindow";
-import ChatInput from "@/components/chat/ChatInput";
+import TaskPanel from "@/components/admin/TaskPanel";
 
-function ChatContent() {
+export default function ChatPage() {
   const router = useRouter();
   const { isAdmin, isLoading } = useAdmin();
-  const { connect, isConnected } = useMcpChat();
 
   useEffect(() => {
     if (!isLoading && !isAdmin) {
       router.push("/login");
     }
   }, [isLoading, isAdmin, router]);
-
-  useEffect(() => {
-    if (isAdmin && !isConnected) {
-      connect();
-    }
-  }, [isAdmin, isConnected, connect]);
 
   if (isLoading) {
     return (
@@ -39,23 +29,9 @@ function ChatContent() {
 
   return (
     <div className="min-h-screen flex bg-[var(--bg)]">
-      <div className="flex-1 flex flex-col max-w-6xl mx-auto">
-        <div className="flex-1 flex overflow-hidden rounded-2xl bg-[var(--card)] border border-[var(--border)] m-4">
-          <ChatSidebar />
-          <div className="flex-1 flex flex-col">
-            <ChatWindow />
-            <ChatInput />
-          </div>
-        </div>
+      <div className="flex-1 flex flex-col max-w-4xl mx-auto p-4">
+        <TaskPanel />
       </div>
     </div>
-  );
-}
-
-export default function ChatPage() {
-  return (
-    <McpChatProvider>
-      <ChatContent />
-    </McpChatProvider>
   );
 }
