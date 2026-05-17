589fe428355befc024600af852c8444256e74c05
yechayo <2628505364@qq.com>
2026-05-17 15:26:42 +0800

    换用 animal-island-ui 风格 cursor 和叶子 favicon

--- src/components/layout/NativeCursor.tsx
+++ src/components/layout/NativeCursor.tsx
@@ -1,12 +1,5 @@
 "use client";
 
-import { useEffect } from "react";
-
 export default function NativeCursor() {
-  useEffect(() => {
-    document.documentElement.style.cursor = "";
-    document.body.style.cursor = "";
-  }, []);
-
   return null;
 }
